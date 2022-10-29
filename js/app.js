bcb57d407d7cd0ad0fdd4df3a217220ce14fdfe2
Adrian Hernandez <adrian.hernandez1993@yahoo.com>
2022-10-28 20:11:38 -0700

    Added addArray function

--- js/app.js
+++ js/app.js
@@ -60,7 +60,7 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
 testSumAndMultiply(4,7,5);
-console.log(testSumAndMultiply);
+// console.log(testSumAndMultiply);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -78,12 +78,15 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let sumArrayOne = sum(sumArr[0], sumArr[1]);
+  let sumArrayFinal = sum(sumArrayOne[0], sumArr[2]);
+  return[sumArrayFinal[0], `2,3,4 was passed in as an array of numbers, and ${sumArrayFinal[0]} is their sum.`];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
+console.log(testSumArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -103,7 +106,8 @@ function multiplyArray(multArr) { //eslint-disable-line
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
