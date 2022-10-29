202ba404eeb01fb2922cb9db63e46a23dacac97e
Adrian Hernandez <adrian.hernandez1993@yahoo.com>
2022-10-28 20:20:12 -0700

    Added multiplyArray function

--- js/app.js
+++ js/app.js
@@ -86,7 +86,7 @@ function sumArray(sumArr) { //eslint-disable-line
 // Here is the test for sumArray(); uncomment it to run it
 
 testSumArray(testArray);
-console.log(testSumArray);
+// console.log(testSumArray)
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -102,11 +102,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+  let productArrOne = multiply(multArr[0], multArr[1]);
+  let productArrFinal = multiply(productArrOne[0], multArr[2]);
+  return[productArrFinal[0], `The numbers 2,3,4 have a product of ${productArrFinal[0]}.`];
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
 testMultiplyArray(testArray);
+// console.log(testMultiplyArray);
 
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
